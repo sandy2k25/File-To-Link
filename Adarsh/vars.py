416df55a23871d9640f4703f4da4066e77422c39
sandy2k25 <sandy_2k25@proton.me>
2025-01-30 11:45:35 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -22,7 +22,7 @@ class Var(object):
     OWNER_ID = set(int(x) for x in os.environ.get("5287262394", "").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str(getenv('offline_noone'))
     if 'DYNO' in environ:OWNER_USERNAME
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
